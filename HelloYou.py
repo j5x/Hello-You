1e72229f3879d8f9e3be7550092ffef588f0a69d
j5x <32881@ma-web.nl>
2021-11-05 09:39:38 +0100

    fc

--- HelloYou.py
+++ HelloYou.py
@@ -1,28 +1,10 @@
 import os
 play = True
-Q1 = True
-Q2 = False
-Q3 = False
-Q4 = False
-Q5 = False
-Q6 = False
-Q7 = False
-Q8 = False
-Q9 = False
-Q10 = False
-Q11 = False
-Q12 = False
-Q13 = False
-Q14 = False
-Q15 = False
-Q16 = False
-Q17 = False
-Q18 = False
-Q19 = False
-Q20 = False
-Q21 = False
+
+Q = 1
+
 while play:
-    while Q1:
+    while Q == 1:
         print("Jij bent Ahmad Sawari, je woont in Syrie(Let op gebruik HOOFDLETTERS!)")
         print("Je zit lekker TV te kijken met je familie en ineens hoor je een luide knal.")
         print("Wat ga je doen?")
@@ -31,15 +13,13 @@ while play:
         print("")
         InputText = input().capitalize()
         if InputText == "A":
-            Q1 = False
-            Q2 = True
+            Q == 2
             os.system('cls')
         if InputText == "B":
-            Q1 = False
-            Q3 = True
+            Q == 3
             os.system('cls')
 
-    while Q3:
+    while Q == 3:
         print("Als je buiten bent zie je het Syrische Leger.")
         print("")
         print("Wat ga je doen?")
@@ -48,26 +28,23 @@ while play:
         print("")
         InputText = input().capitalize()
         if InputText == "A":
-            Q3 = False
-            Q5 = True
+            Q == 5
             os.system('cls')
         if InputText == "B":
-            Q7 = True
-            Q3 = False
+            Q == 7
             os.system('cls')
 
-    while Q5:
+    while Q == 5:
         print("Je bent het leger ontsnapt, en je gaat richting het vliegveld.")
         print("Als je eenmaal aankomt bij het vliegveld besluit je om naar Nederland te gaan.")
         print("Je hebt toestemming om naar Nederland te vluchten en je bent nu onderweg naar het vliegveld.")
         print("Type: A to continue...")
         InputText = input().capitalize()
         if InputText == "A":
-            Q5 = False
-            Q13 = True
+            Q == 13
             os.system('cls')
 
-    while Q7:
+    while Q == 7:
         print("Je rent naar binnen om je familie te halen.")
         print("Jullie rennen via de achter deur naar buiten en lopen om naar de auto.")
         print("In de auto krijgen jullie 2 opties.")
@@ -76,15 +53,13 @@ while play:
         print("")
         InputText = input().capitalize()
         if InputText == "A":
-            Q7 = False
-            Q9 = True
+            Q == 7
             os.system('cls')
         if InputText == "B":
-            Q7 = False
-            Q11 = True
+            Q == 11
             os.system('cls')
 
-    while Q9:
+    while Q== 9:
         print("Jullie komen aan bij de bij het vliegveld.")
         print("Je gaat naar de balie toe en vraagt of jullie naar Nederland mogen vluchten.")
         print("Jullie hebben toestemming gekregen van Nederland om daar heen te vluchten")
@@ -92,11 +67,10 @@ while play:
         print("Type: A to continue...")
         InputText = input().capitalize()
         if InputText == "A":
-            Q9 = False
-            Q15 = True
+            Q == 15
             os.system('cls')
 
-    while Q11:
+    while Q == 11:
         print("Jullie komen bij de grens aan")
         print("Jullie worden gestopt door de duane")
         print("De duane ondervraagt jou.")
@@ -104,11 +78,10 @@ while play:
         print("Type: A to continue...")
         InputText = input().capitalize()
         if InputText == "A":
-            Q11 = False
-            Q17 = True
+            Q == 17
             os.system('cls')
 
-    while Q13:
+    while Q == 13:
         print("")
         print("Op het vliegveld kom je je familie tegen.")
         print("Ze zijn met iemand mee gelift om bij het vliegveld te komen.")
@@ -118,16 +91,14 @@ while play:
         print("B: Jullie pakken het 2e vliegtuig.")
         InputText = input().capitalize()
         if InputText == "A":
-            Q13 = False
-            Q15 = True
+            Q == 15
             os.system('cls')
             print("Type: A to continue...")
         InputText = input().capitalize()
         if InputText == "B":
-            Q13= False
-            Q12 = True
+            Q == 12
             os.system('cls')
-    while Q15:
+    while Q== 15:
         print("Het vliegtuig is er en jullie gaan aanboord. ")
         print("Eenmaal aanboord zoeken jullie een goede plek uit.")
         print("")
@@ -136,21 +107,19 @@ while play:
         print("Type: A to continue...")
         InputText = input().capitalize()
         if InputText == "A":
-            Q15 = False
-            Q19 = True
+            Q == 19
 
-    while Q17:
+    while Q == 17:
         print("Je krijgt levenslang vanwege land verraad.")
         print("Je familie probeert nog te onstnappen")
         print("Je familie wordt gepakt.")
         print("Type: A to continue")
         InputText = input().capitalize()
         if InputText == "A":
-            Q17 = False
-            Q21 = True
+            Q == 21
             os.system('cls')
 
-    while Q19:
+    while Q == 19:
         print("")
         print("Hij heeft een bomgordel om zich heen")
         print("Iedereen aan boord probeerd hem over te halen om het niet te doen.")
@@ -162,8 +131,7 @@ while play:
         print("")
         scriptinput = input().capitalize()
         if scriptinput == "y" or scriptinput == "Y":
-            Q19 = False
-            Q1 = True
+            Q == 1
             print("[o]*Het script word gerestart*")
             print("")
             os.system('cls')
@@ -172,7 +140,7 @@ while play:
             os.system('cls')
             exit()
         
-    while Q21:
+    while Q == 21:
         print("Je familie is gepakt en jullie worden allemaal gestraft.")
         print("De overheid heeft besloten jullie te excuteren.")
         print("GAME OVER")
@@ -189,7 +157,7 @@ while play:
             os.system('cls')
             exit()
 
-    while Q2:
+    while Q == 2:
         print("Jullie horen allemaal schoten.")
         print("Het is het Syrische leger.")
         print("Jullie gaan naar de auto.")
@@ -198,14 +166,12 @@ while play:
         print("B: het vliegveld")
         InputText = input().capitalize()
         if InputText == "A":
-            Q4 = True
-            Q2 = False
+            Q == 4
             os.system('cls')
         if InputText == "B":
-            Q6 = True
-            Q2 = False
+            Q == 6
             os.system('cls')
-    while Q4:
+    while Q == 4:
         print("Jullie komen aan bij de haven.")
         print("Jullie komen een oude vriend tegen.")
         print("Jullie praten even met hem en vertellen hem wat er is gebeurd.")
@@ -217,24 +183,21 @@ while play:
         print("B: Nederland")
         InputText = input().capitalize()
         if InputText == "A":
-            Q4 = False
-            Q8 = True
+            Q == 8
         if InputText == "B":
-            Q4 = False
-            Q10 = True
+            Q == 10
 
-    while Q6:
+    while Q == 6:
         print("Jullie komen aan op het vliegveld")
         print("Jullie gaan naar de balie en alleen jij krijgt toestemming van Nederland om er heen te vluchten.")
         print("Je gaat in discussie met de man achter de balie.")
         print("Type: A to continue... ")
         InputText = input().capitalize()
         if InputText == "A":
-            Q6 = False
-            Q12 = True
+            Q == 12
             os.system('cls')
 
-    while Q8:
+    while Q == 8:
         print("Jullie zijn onderweg naar Griekeland.")
         print("Onderweg vraagt hij of je zeker weten naar Griekeland wilt.")
         print("Hij zegt dat Nederland een.")
@@ -245,11 +208,9 @@ while play:
         print("B: Nederland")
         InputText = input().capitalize()
         if InputText == "A":
-            Q8 = False
-            Q14 = True
+            Q == 14
         if InputText == "B": 
-            Q8 = False
-            Q16 = True
+            Q == 16
 
-    while Q10:
+    while Q == 10:
         print("")
